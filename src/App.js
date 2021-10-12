68be3acca6a5aa1049cce998a5b14b5034d8c31b
Pedro Siqueira <pedro.v.siqueira@gmail.com>
2021-10-12 19:48:55 -0300

    feat: create initial context for application

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import { constants, theme } from 'utils';
 import { ThemeProvider } from 'styled-components';
 import GlobalStyle from './styles/global';
+import { useContext, useEffect } from 'react';
+import { PokemonContext } from 'hooks/pokemonContext';
+import { ToastContainer } from 'react-toastify';
 
 function App() {
+  const { fetchPokemonByType } = useContext(PokemonContext);
+
+  useEffect(() => {
+    fetchPokemonByType();
+  }, [fetchPokemonByType]);
+
   return (
     <ThemeProvider theme={theme}>
       <h1 style={{ color: theme.colors.primary }}>{constants.TITLE}</h1>
       <GlobalStyle />
+      <ToastContainer />
     </ThemeProvider>
   );
 }
---
+++ src/hooks/pokemonContext.js
@@ -0,0 +1,49 @@
+import React, { createContext, useCallback, useState } from 'react';
+import { toast } from 'react-toastify';
+import { api } from '../services/api';
+import { constants } from 'utils';
+
+const PokemonContext = createContext();
+
+const ContextProvider = ({ children }) => {
+  const [pokemons, setPokemons] = useState([]);
+
+  const toastMessageDefaults = {
+    position: 'bottom-left',
+    autoClose: 3000,
+    hideProgressBar: true,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true
+  };
+
+  const notifySuccess = message => {
+    toast.success(message, { ...toastMessageDefaults });
+  };
+
+  const notifyError = message => {
+    toast.error(message, { ...toastMessageDefaults });
+  };
+
+  const fetchPokemonByType = useCallback(async () => {
+    try {
+      const result = await api.get(constants.TYPE);
+      setPokemons(result.data.pokemon);
+      // notifySuccess('Pokemon loaded successfully');
+    } catch {
+      // notifyError('Error while loading the data');
+    }
+  }, []); //eslint-disable-line
+
+  return (
+    <PokemonContext.Provider
+      value={{
+        fetchPokemonByType
+      }}
+    >
+      {children}
+    </PokemonContext.Provider>
+  );
+};
+
+export { ContextProvider, PokemonContext };
