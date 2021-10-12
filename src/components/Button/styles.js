34ebc47dc04d3d7a4eafe134830869a03e4c6631
Pedro Siqueira <pedro.v.siqueira@gmail.com>
2021-10-12 20:27:38 -0300

    feat: create styles for button component

---
+++ src/components/Button/styles.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const StyledButton = styled.button`
+  font-size: 1rem;
+  width: 11rem;
+  color: #fff;
+  padding: 0.6rem;
+  text-align: center;
+  border: none;
+  border-radius: 5px;
+  transition: background-color 200ms linear;
+  background-color: ${props => props.backgroundColor};
+
+  :hover {
+    filter: brightness(90%);
+  }
+`;
