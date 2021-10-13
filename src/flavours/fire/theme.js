1e31c3b5717c93776ab48a5596eed806b8c9aab4
Pedro Siqueira <pedro.v.siqueira@gmail.com>
2021-10-12 21:17:03 -0300

    feat: add new theme colors for input

--- src/flavours/fire/theme.js
+++ src/flavours/fire/theme.js
@@ -3,7 +3,12 @@ const theme = {
     backgroundColor: '#272727',
     textColor: '#FFF',
     detailsButtonColor: 'pink',
-    addButtonColor: 'green'
+    addButtonColor: 'green',
+    inputBackgroundColor: '#f5f4f6',
+    inputBackgroundColorFocused: '#dedce1',
+    inputTextPlaceholder: '#b1adb9',
+    inputTextFocused: '#8f8a9b',
+    inputText: '#170c3a'
   }
 };
 
