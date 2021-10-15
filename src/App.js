fce02e67dc624dea3c43a89901572a50f7c95280
Pedro Siqueira <pedro.v.siqueira@gmail.com>
2021-10-15 11:39:41 -0300

    fix: import toastify css

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { ThemeProvider } from 'styled-components';
 import GlobalStyle from './styles/global';
 import { ToastContainer } from 'react-toastify';
 import { Home } from 'pages/Home/Home';
+import 'react-toastify/dist/ReactToastify.min.css';
 
 function App() {
   return (
