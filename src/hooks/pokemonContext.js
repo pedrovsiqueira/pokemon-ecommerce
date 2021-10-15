c20b7583b2234dc9e9e6b1003903a21bdd81a6f1
Pedro Siqueira <pedro.v.siqueira@gmail.com>
2021-10-15 11:38:38 -0300

    feat: add local storage to context

--- src/hooks/pokemonContext.js
+++ src/hooks/pokemonContext.js
@@ -1,17 +1,37 @@
-import React, { createContext, useCallback, useState } from 'react';
+import React, { createContext, useCallback, useEffect, useMemo, useState } from 'react';
 import { toast } from 'react-toastify';
 import { api } from '../services/api';
 import { constants } from 'utils';
-import { generatePrice, pokemonImageUrl } from 'utils/helpers';
+import { formatPokemonData } from 'utils/helpers';
 
 const PokemonContext = createContext();
 
 const ContextProvider = ({ children }) => {
   const [pokemons, setPokemons] = useState([]);
-  const [search, setSearch] = useState([]);
+  const [filteredPokemon, setFilteredPokemon] = useState([]);
+  const [search, setSearch] = useState('');
   const [loading, setLoading] = useState(false);
   const [triggerDetailsModal, setTriggerDetailsModal] = useState(false);
+  const [triggerSubmitModal, setTriggerSubmitModal] = useState(false);
   const [currentPokemon, setCurrentPokemon] = useState({});
+  const [cartItems, setCartItems] = useState(() => {
+    try {
+      const data = JSON.parse(localStorage.getItem('cartItems'));
+      return Array.isArray(data) ? data : [];
+    } catch {
+      return [];
+    }
+  });
+  const [openCart, setOpenCart] = useState(false);
+
+  useEffect(() => {
+    const result = pokemons.filter(pokemon => pokemon.name.toLowerCase().includes(search));
+    setFilteredPokemon(result);
+  }, [pokemons, search]);
+
+  useEffect(() => {
+    localStorage.setItem('cartItems', JSON.stringify(cartItems));
+  }, [cartItems]);
 
   const toastMessageDefaults = {
     position: 'bottom-left',
@@ -31,39 +51,104 @@ const ContextProvider = ({ children }) => {
   };
 
   const fetchPokemonData = useCallback(async pokemon => {
-    let { url } = pokemon.pokemon;
-    url = `pokemon/${url.substring(34)}`;
-
+    setLoading(true);
+    let { url } = pokemon;
     try {
       const result = await api.get(url);
-      const { id, weight, species, types, height } = result.data;
-      const pokemonData = {
-        id,
-        name: species.name.toUpperCase(),
-        image: `${pokemonImageUrl}${id}.png`,
-        price: generatePrice(id),
-        types,
-        weight,
-        height
-      };
-      setPokemons(prevPokemons => [...prevPokemons, pokemonData]);
-    } catch {
-      // notifyError('Error while loading the data');
+      const pokemonData = formatPokemonData(result.data);
+      setCurrentPokemon(pokemonData);
+    } catch (error) {
+      notifyError('Erro ao carregar os dados');
     } finally {
       setLoading(false);
     }
-  }, []);
+  }, []); //eslint-disable-line
 
   const fetchPokemonByType = useCallback(async () => {
     setLoading(true);
     try {
       const result = await api.get(`type/${constants.TYPE}`);
-      result.data.pokemon.forEach(pokemon => fetchPokemonData(pokemon));
-      // notifySuccess('Pokemon loaded successfully');
-    } catch {
-      // notifyError('Error while loading the data');
+      const formattedPokemonData = result.data.pokemon.map(pokemon =>
+        formatPokemonData(pokemon.pokemon)
+      );
+
+      setPokemons(formattedPokemonData);
+    } catch (error) {
+      notifyError('Erro ao carregar os dados');
+    } finally {
+      setLoading(false);
     }
   }, []); //eslint-disable-line
+
+  const handleAddToCart = pokemon => {
+    const { id } = pokemon;
+    const product = cartItems.find(item => item.id === id);
+    const productAmount = (product?.amount ?? 0) + 1;
+
+    if (product) {
+      const updatedCart = cartItems.map(item => {
+        if (item.id === id) {
+          item.amount = productAmount;
+        }
+
+        return item;
+      });
+
+      setOpenCart(true);
+      return setCartItems(updatedCart);
+    }
+
+    const updatedCart = [...cartItems, { ...pokemon, amount: 1 }];
+
+    setCartItems(updatedCart);
+    setOpenCart(true);
+    notifySuccess('Pokemon adicionado ao carrinho');
+  };
+
+  const handleRemoveFromCart = pokemonId => {
+    try {
+      const updatedCart = cartItems.filter(item => item.id !== pokemonId);
+      setCartItems(updatedCart);
+      notifySuccess('Pokemon removido com sucesso');
+    } catch {
+      notifyError('Erro ao remover do carrinho');
+    }
+  };
+
+  const handleAmountChange = (operation, id, amount) => {
+    if (operation === '-' && amount === 1) {
+      return handleRemoveFromCart(id);
+    }
+
+    const updatedAmount = operation === '+' ? ++amount : --amount;
+    const updatedCart = cartItems.map(item => {
+      if (item.id === id) {
+        item.amount = updatedAmount;
+      }
+
+      return item;
+    });
+
+    setCartItems(updatedCart);
+  };
+
+  const handleSubmit = () => {
+    try {
+      setOpenCart(false);
+      setTriggerSubmitModal(true);
+      notifySuccess('Compra finalizada com sucesso');
+    } catch {
+      notifyError('Erro ao finalizar compra');
+    }
+  };
+
+  const closeSubmit = () => {
+    setCartItems([]);
+    setCurrentPokemon({});
+    setTriggerSubmitModal(false);
+  };
+
+  const total = useMemo(() => cartItems.reduce((a, b) => a + b.price * b.amount, 0), [cartItems]);
 
   return (
     <PokemonContext.Provider
@@ -76,7 +161,19 @@ const ContextProvider = ({ children }) => {
         triggerDetailsModal,
         setTriggerDetailsModal,
         currentPokemon,
-        setCurrentPokemon
+        fetchPokemonData,
+        cartItems,
+        handleAddToCart,
+        openCart,
+        setOpenCart,
+        handleRemoveFromCart,
+        handleAmountChange,
+        triggerSubmitModal,
+        handleSubmit,
+        setTriggerSubmitModal,
+        closeSubmit,
+        filteredPokemon,
+        total
       }}
     >
       {children}
