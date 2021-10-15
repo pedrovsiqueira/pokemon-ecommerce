e45ade5d2db7e5abde885d610378fdb26d76e91a
Pedro Siqueira <pedro.v.siqueira@gmail.com>
2021-10-15 11:37:03 -0300

    fix: update fire flavor

--- src/flavours/fire/theme.js
+++ src/flavours/fire/theme.js
@@ -1,15 +1,14 @@
 const theme = {
   colors: {
     textColor: '#F47A32',
-    detailsButtonColor: '#F47A32',
+    primaryButtonColor: '#F47A32',
     backgroundColor: '#F47A32',
-    addButtonColor: '#170c3a',
+    secondaryButtonColor: '#170c3a',
     inputBackgroundColor: '#f5f4f6',
     inputBackgroundColorFocused: '#dedce1',
     inputTextPlaceholder: '#b1adb9',
     inputTextFocused: '#8f8a9b',
-    inputText: '#170c3a',
-    cardBackgroundColor: '#ffbf99'
+    inputText: '#170c3a'
   }
 };
 
