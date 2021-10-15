e7d05371fcd8f6183ecadf12828d9cbdfdf7b067
Pedro Siqueira <pedro.v.siqueira@gmail.com>
2021-10-15 11:37:35 -0300

    fix: update modal component to handle multiple data

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,14 +1,13 @@
 import { ButtonIcon } from 'components/Button/ButtonIcon';
 import { PokemonContext } from 'hooks/pokemonContext';
+import { theme } from 'utils';
 import { useContext } from 'react';
 import ReactModal from 'react-modal';
 import { ReactComponent as CloseIcon } from 'assets/close.svg';
-import { Container, Heading, SubHeading, Body } from './styles';
-import { constants } from 'utils';
+import { FallbackLoader } from 'FallbackLoader/FallbackLoader';
 
-export const Modal = ({ isOpen, onRequestClose, className }) => {
-  const { currentPokemon } = useContext(PokemonContext);
-  const { image, name, price, weight, height, id } = currentPokemon;
+export const Modal = ({ isOpen, onRequestClose, className, children }) => {
+  const { loading } = useContext(PokemonContext);
 
   return (
     <ReactModal
@@ -18,33 +17,10 @@ export const Modal = ({ isOpen, onRequestClose, className }) => {
       className={className}
       ariaHideApp={false}
     >
-      <Container>
-        <Heading>{name}</Heading>
-        <SubHeading>{price}</SubHeading>
+      {children}
+      <ButtonIcon onClick={onRequestClose} Icon={CloseIcon} className="btn--close-modal" />
 
-        <img src={image} alt={`Pokemon ${name}`} />
-
-        <Body>
-          <div>
-            <p>
-              <strong>Peso:</strong> {weight}kg
-            </p>
-            <p>
-              <strong>Altura:</strong> {height}m
-            </p>
-          </div>
-          <div>
-            <p>
-              <strong>Id:</strong> {id}
-            </p>
-            <p>
-              <strong>Tipo:</strong> {constants.TRANSLATED_TYPE}
-            </p>
-          </div>
-        </Body>
-      </Container>
-
-      <ButtonIcon onClick={onRequestClose} Icon={CloseIcon} />
+      <FallbackLoader visible={loading} color={theme.colors.textColor} />
     </ReactModal>
   );
 };
