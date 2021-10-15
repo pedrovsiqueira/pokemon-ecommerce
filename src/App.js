ea3a73fab1ceaf63b85b51599769df6fa58e6c82
Pedro Siqueira <pedro.v.siqueira@gmail.com>
2021-10-15 11:56:34 -0300

    feat: update document title dynamically

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import { theme } from 'utils';
+import { theme, constants } from 'utils';
 import { ThemeProvider } from 'styled-components';
 import GlobalStyle from './styles/global';
 import { ToastContainer } from 'react-toastify';
 import { Home } from 'pages/Home/Home';
 import 'react-toastify/dist/ReactToastify.min.css';
+import { useEffect } from 'react';
 
 function App() {
+  useEffect(() => {
+    document.title = constants.NAME;
+  }, []);
+
   return (
     <ThemeProvider theme={theme}>
       <Home />
