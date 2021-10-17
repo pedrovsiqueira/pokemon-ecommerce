76d855ae7a3062a9c8a7b0340b6f58223d45059f
Pedro Siqueira <pedro.v.siqueira@gmail.com>
2021-10-17 17:00:16 -0300

    fix: remove unused name prop

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -1,13 +1,12 @@
 import { ReactComponent as SearchIcon } from 'assets/search.svg';
 import { Container } from './styles';
 
-export const Input = ({ name, placeholder, onChange, value }) => (
+export const Input = ({ placeholder, onChange, value }) => (
   <Container className={`search-field`}>
     <SearchIcon className="search-field-icon" />
     <input
       value={value}
       type="search"
-      name={name}
       className={`field__input search-field__input`}
       placeholder={placeholder}
       onChange={onChange}
