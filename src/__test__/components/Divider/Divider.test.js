fac939b4169673d2358308eb3ebcf9de9a6d8192
Pedro Siqueira <pedro.v.siqueira@gmail.com>
2021-10-17 22:13:46 -0300

    feat: add divider component test

--- src/__test__/components/Divider/Divider.test.js
+++ src/__test__/components/Divider/Divider.test.js
@@ -1,5 +1,5 @@
-const { render } = require('@testing-library/react');
-const { Divider } = require('components/Divider/Divider');
+import { render } from '@testing-library/react';
+import { Divider } from 'components/Divider/Divider';
 
 test('Should render Divider without crashing', () => {
   render(<Divider />);
